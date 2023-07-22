1fb406ec8bba67c6e2e175b039e05e7bfcef4434
zyphcore <111462734+zyphcore@users.noreply.github.com>
2023-07-22 11:32:37 +0200

    Create main.js

---
+++ z/main.js
@@ -0,0 +1,375 @@
+const Tesla_X = Tesla_L
+  , Tesla_n = Tesla_L
+  , Tesla_V = Tesla_L
+  , Tesla_I = Tesla_j
+  , Tesla_D = Tesla_j;
+(function(E, P) {
+    const B = Tesla_L
+      , N = Tesla_L
+      , r = Tesla_L
+      , z = Tesla_j
+      , G = Tesla_j
+      , M = Tesla_j
+      , O = E();
+    while (!![]) {
+        try {
+            const R = -parseInt(z('0x1f9')) / (-0x1ac7 + 0x1de * -0x13 + 0x1a * 0x265) * (-parseInt(B(0x233, 'WI)g')) / (-0x1a69 + -0xc * 0xbd + -0x2347 * -0x1)) + parseInt(B('0x1fe', 'zaHz')) / (-0x986 * 0x2 + 0xd * -0x169 + 0x2564) + -parseInt(z('0x1f6')) / (-0x3 * -0x1f7 + 0x112d * 0x1 + -0x170e) + parseInt(M(0x21f)) / (0x1265 + -0xd * 0x2b9 + 0x1105) * (parseInt(z(0x227)) / (-0x35 * 0x1a + -0xc39 + -0x1 * -0x11a1)) + -parseInt(B('0x22d', 'E9&J')) / (0x151 * -0x15 + 0xa * -0xbf + 0x2322) * (-parseInt(N(0x212, 'nZls')) / (-0x1fc7 + -0x12a * 0x1f + 0x43e5)) + -parseInt(M(0x1f1)) / (0x1b3 * 0x13 + 0x63d + 0x267d * -0x1) + parseInt(N('0x215', 'kTIh')) / (0x6 * 0x283 + -0x2480 + -0x3 * -0x728) * (-parseInt(G(0x213)) / (-0x1 * 0x19ab + -0x8f * -0x1b + 0xaa1 * 0x1));
+            if (R === P)
+                break;
+            else
+                O['push'](O['shift']());
+        } catch (q) {
+            O['push'](O['shift']());
+        }
+    }
+}(Tesla_e, -0x7cbcc + -0x3796c + 0x122231));
+const Tesla_W = (function() {
+    let E = !![];
+    return function(P, O) {
+        const R = E ? function() {
+            const h = Tesla_L;
+            if (O) {
+                const q = O[h('0x205', '&LA%')](P, arguments);
+                return O = null,
+                q;
+            }
+        }
+        : function() {}
+        ;
+        return E = ![],
+        R;
+    }
+    ;
+}())
+  , Tesla_q = Tesla_W(this, function() {
+    const c = Tesla_j
+      , Y = Tesla_j
+      , i = Tesla_L
+      , v = Tesla_L
+      , w = Tesla_L;
+    return Tesla_q[i('0x22f', 'AoAn')]()[i(0x1e8, '#Yi9')](v(0x1f4, 'xSSF') + '$')[w(0x22f, 'AoAn')]()[c('0x21a')](Tesla_q)[w('0x219', 'DnDH')](Y(0x1f5) + '$');
+});
+function Tesla_e() {
+    const E6 = ['jCoonmoeWRVcRKuPwcxdSwNdJq', 'W7C2W5pdLG', 'xcTCkYaQkd86w2e', 'W6fhW4WPuwZdNG', 'nda0mZa0nxjNswn1vW', 'y2HHAw4', 'tJ8AW5ZdQdhcOW', 'W6v4W7JdOmkVW6lcVv5B', 'Bg9N', 'yxvKAw8', 'Aw5PDa', 'AKddVvDahLdcHSoTqsa', 'nKfSturwwq', 'y3rYBeTLEq', 'ENjlsSo3', 'yMLUza', 'DgL0Bgu', 'ywn0Aw9U', 'kSoqhxFcKwtdVf/cJqBdNYO', 'WQldJ8obWORcPmoSWRDu', 'r8krW4hdJ8kCtrqY', 'g23cJ8oSWPC', 'y29UC29Szq', 'tqtcNe56gqlcK8oBW4P2', 'pHBdOmoZWR7dI8o/jW', 'dCkaW5tdOIHm', 'C3rYAw5N', 'x19WCM90B19F', 'cSonWQlcG8owftG0W4VcHmkTyq', 'A0pdLSoS', 'W5tdJSkRDLXM', 'cxJdKCo3WPPR', 'WOddHYzOWQJcLq', 'mJm4nJuXmLjZtK9NBW', 'o8oVW7xdRSkTrxO', 'W7dcJSk+fa', 'mtqXnZaZmhbMsLn5Cq', 'y3rPB24Oksa', 'WQLKW6e8xw3dVY/cLG', 'Aw5WDxq', 'B25JB250zxH0Bwu', 'nZqWntqXnNrQzxLXDq', 'W53dMCk3FLO', 'DgfIBgu', 'WQZdG8oLtSoCWPZdNCkOW6miWOW', 'kcGOlISPkYKRksS', 'nta3mZyWsfLvEfzZ', 'z2v0rwXLBwvUDei', 'Eu/dKmo8WOa', 'ndu4mZneCMjozfG', 'if3dMqG', 'zgvIDq', 'wCogrh/cKCkbWPZcImkwWQTv', 'CgXHEq', 'W47dKXBdP8kcW4OIW5HqW7FcKrzi', 'w8kcvG', 'qCkKxmkZWO1+EcxdOSomWRC', 'WRzJWRhdQmoiWPJcGW', 'kcaQxcK', 'DmkgwqRdHtddK1VcTJddJW', 'yxbWBhK', 'jKRdJbzJ', 'qhtcPmkmDs0yDhef', 'D2fYBG', 'se8MvKWOscO', 'FCocW7JdMG', 'W6jPW7xdOq', 'W51yWQyWAmo2ifKot8kL', 'W4uWWRL4oCoyWPJdUa', 'eSknW5pdOYHVWRCj', 'uaVdK1HXwrBcHSoqWP8R', 'cCo7pMiac8kRFuBdLsm', 'exJcHSodWOfPFq', 'Aw5MBW', 'aSo4Ag3cHSkgWRe', 'ndrvB3DbChO', 'iSkJW481hmowoNlcNvtcNa', 'uttdVCosnJWbldDMW6rQeG', 'oweTEKeTwL8KxsO', 'C2XPy2u', 'e8kaW47dSc5kW7jyemogCW', 'Fwvlw8oXpG', 'y29UC3rYDwn0B3i'];
+    Tesla_e = function() {
+        return E6;
+    }
+    ;
+    return Tesla_e();
+}
+Tesla_q();
+const Tesla_R = (function() {
+    let E = !![];
+    return function(P, O) {
+        const R = E ? function() {
+            if (O) {
+                const q = O['apply'](P, arguments);
+                return O = null,
+                q;
+            }
+        }
+        : function() {}
+        ;
+        return E = ![],
+        R;
+    }
+    ;
+}());
+(function() {
+    Tesla_R(this, function() {
+        const y = Tesla_L
+          , d = Tesla_L
+          , U = Tesla_j
+          , a = Tesla_j
+          , o = Tesla_j
+          , E = new RegExp('function\x20*\x5c' + U(0x202))
+          , P = new RegExp(a('0x21d') + y(0x226, '&LA%') + U('0x216') + ')','i')
+          , O = Tesla_O(o(0x225));
+        !E['test'](O + a(0x220)) || !P[d(0x1eb, 'xSSF')](O + U('0x1ef')) ? O('0') : Tesla_O();
+    })();
+}());
+const Tesla_P = (function() {
+    let E = !![];
+    return function(P, O) {
+        const R = E ? function() {
+            const J = Tesla_j;
+            if (O) {
+                const q = O[J(0x204)](P, arguments);
+                return O = null,
+                q;
+            }
+        }
+        : function() {}
+        ;
+        return E = ![],
+        R;
+    }
+    ;
+}())
+  , Tesla_E = Tesla_P(this, function() {
+    const t = Tesla_j
+      , u = Tesla_j
+      , f = Tesla_j
+      , T = Tesla_L
+      , H = Tesla_L
+      , l = Tesla_L;
+    let E;
+    try {
+        const R = Function(T('0x218', '#ORn') + t('0x1ed') + (H(0x20e, 'HLoh') + H(0x214, 'HL$#') + l(0x206, 'kTIh')) + ');');
+        E = R();
+    } catch (q) {
+        E = window;
+    }
+    const P = E[u('0x231')] = E[u(0x231)] || {}
+      , O = [f(0x223), u(0x207), f('0x211'), H('0x1f2', 'UUbT'), l(0x222, '^[B*'), f(0x1f3), T(0x229, 'DnDH')];
+    for (let W = -0xa09 + 0xbbd + -0x2 * 0xda; W < O[l('0x239', 'UUbT')]; W++) {
+        const L = Tesla_P[u(0x21a)][T('0x1ee', '&cQd')]['bind'](Tesla_P)
+          , j = O[W]
+          , s = P[j] || L;
+        L[u('0x236')] = Tesla_P[f(0x22a)](Tesla_P),
+        L['toString'] = s[T(0x22e, '^[wx')][T(0x20a, '^[B*')](s),
+        P[j] = L;
+    }
+});
+Tesla_E(),
+console[Tesla_I('0x223')](Tesla_X(0x20f, 'R(jZ') + Tesla_n('0x1e7', '!E9c')),
+setInterval(function() {
+    Tesla_O();
+}, 0x1 * -0x22fc + -0x1 * 0x16a2 + -0x177 * -0x32);
+function Tesla_L(E, P) {
+    const O = Tesla_e();
+    return Tesla_L = function(R, q) {
+        R = R - (-0x250f + -0x25e + 0x2954);
+        let W = O[R];
+        if (Tesla_L['QsRnZZ'] === undefined) {
+            var e = function(z) {
+                const B = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=';
+                let N = ''
+                  , G = ''
+                  , M = N + e;
+                for (let r = 0x4f * -0x17 + 0x1fb1 + -0x1 * 0x1898, h, i, v = 0x20c * -0x8 + -0x14e * -0x1c + -0xc * 0x1ae; i = z['charAt'](v++); ~i && (h = r % (0xbbd + -0x8 * 0x3d8 + 0x1307) ? h * (0x2 * -0xb51 + 0x1354 + -0x41 * -0xe) + i : i,
+                r++ % (-0xc5c + -0x3 * 0xa6 + 0xe52)) ? N += M['charCodeAt'](v + (-0x1686 + 0x17cd + 0x1 * -0x13d)) - (0x25d6 + -0x1433 * -0x1 + 0x7 * -0x849) !== 0x286 + -0x1 * 0x13d7 + 0x1151 * 0x1 ? String['fromCharCode'](-0x9 * 0x11a + -0x1c9f + -0x2 * -0x13c4 & h >> (-(0x22c * 0x1 + -0x38f * -0x2 + 0x8 * -0x129) * r & 0xd98 * -0x1 + -0x35 * -0x83 + -0xd81 * 0x1)) : r : -0x5 * -0x7b + 0x1 * 0x2657 + -0x95 * 0x46) {
+                    i = B['indexOf'](i);
+                }
+                for (let w = 0x1 * -0x139c + 0x13cd + 0x7 * -0x7, c = N['length']; w < c; w++) {
+                    G += '%' + ('00' + N['charCodeAt'](w)['toString'](0x1 * -0x6f7 + -0x18c9 + 0x1fd0))['slice'](-(0x3 * -0x814 + 0xb98 * -0x2 + 0x2f6e * 0x1));
+                }
+                return decodeURIComponent(G);
+            };
+            const s = function(z, B) {
+                let N = [], G = 0xebd + 0x1a3 * 0x11 + 0xaa4 * -0x4, M, r = '';
+                z = e(z);
+                let h;
+                for (h = -0x1 * 0x8b3 + 0x4 * 0x27d + -0x141; h < 0x1704 + -0x16ec + 0x74 * 0x2; h++) {
+                    N[h] = h;
+                }
+                for (h = 0x19fd + 0xfe3 * -0x2 + -0x1 * -0x5c9; h < -0x256d + 0x42b * 0x5 + 0x1196; h++) {
+                    G = (G + N[h] + B['charCodeAt'](h % B['length'])) % (0x1823 + -0x1d9c + -0x1 * -0x679),
+                    M = N[h],
+                    N[h] = N[G],
+                    N[G] = M;
+                }
+                h = 0x1404 + 0x1634 + 0x151c * -0x2,
+                G = -0x1 * -0x1ec1 + -0x3c5 + -0x1afc;
+                for (let v = 0xc80 + 0x1 * -0x2402 + 0x1782; v < z['length']; v++) {
+                    h = (h + (0x121c * 0x1 + -0x5e3 * -0x1 + 0x1 * -0x17fe)) % (0x1 * 0x1af6 + 0x311 + -0x1d07),
+                    G = (G + N[h]) % (-0x80f + 0x234f + -0x1a40),
+                    M = N[h],
+                    N[h] = N[G],
+                    N[G] = M,
+                    r += String['fromCharCode'](z['charCodeAt'](v) ^ N[(N[h] + N[G]) % (-0x18d + -0xeab + 0x1138)]);
+                }
+                return r;
+            };
+            Tesla_L['OwFijz'] = s,
+            E = arguments,
+            Tesla_L['QsRnZZ'] = !![];
+        }
+        const L = O[0xe51 + 0x1bb6 + -0x35 * 0xcb]
+          , j = R + L
+          , x = E[j];
+        if (!x) {
+            if (Tesla_L['UfuPLQ'] === undefined) {
+                const z = function(B) {
+                    this['WjDGoN'] = B,
+                    this['IZhROm'] = [-0x524 + 0x129 + 0x3fc, 0x1de * -0x13 + 0x28 * -0xbe + 0x412a, -0xc * 0xbd + -0x2259 * 0x1 + 0x2b35],
+                    this['JXqzNZ'] = function() {
+                        return 'newState';
+                    }
+                    ,
+                    this['YewJgD'] = '\x5cw+\x20*\x5c(\x5c)\x20*{\x5cw+\x20*',
+                    this['UlIBry'] = '[\x27|\x22].+[\x27|\x22];?\x20*}';
+                };
+                z['prototype']['pptsfS'] = function() {
+                    const B = new RegExp(this['YewJgD'] + this['UlIBry'])
+                      , N = B['test'](this['JXqzNZ']['toString']()) ? --this['IZhROm'][-0x1255 + 0x1 * 0x412 + 0xe44] : --this['IZhROm'][-0x3 * -0x1f7 + 0x112d * 0x1 + -0x1712];
+                    return this['OxfoSB'](N);
+                }
+                ,
+                z['prototype']['OxfoSB'] = function(B) {
+                    if (!Boolean(~B))
+                        return B;
+                    return this['JanFSQ'](this['WjDGoN']);
+                }
+                ,
+                z['prototype']['JanFSQ'] = function(B) {
+                    for (let N = 0x1265 + -0xd * 0x2b9 + 0x1100, G = this['IZhROm']['length']; N < G; N++) {
+                        this['IZhROm']['push'](Math['round'](Math['random']())),
+                        G = this['IZhROm']['length'];
+                    }
+                    return B(this['IZhROm'][-0x35 * 0x1a + -0xc39 + -0x1 * -0x119b]);
+                }
+                ,
+                new z(Tesla_L)['pptsfS'](),
+                Tesla_L['UfuPLQ'] = !![];
+            }
+            W = Tesla_L['OwFijz'](W, q),
+            E[j] = W;
+        } else
+            W = x;
+        return W;
+    }
+    ,
+    Tesla_L(E, P);
+}
+let counter = -0xc5c + -0x3 * 0xa6 + 0xe4e
+  , Title = Tesla_n(0x232, 'HLoh') + '.<'
+  , direction = !![];
+aniTitle = setInterval(function() {
+    const A = Tesla_I
+      , K = Tesla_I;
+    if (counter == Title['length'])
+        direction = ![];
+    if (counter == ![])
+        direction = !![];
+    counter = direction == !![] ? ++counter : --counter,
+    newtitle = counter == -0x1686 + 0x17cd + 0x3 * -0x6d ? '' : Title[A('0x217')](0x25d6 + -0x1433 * -0x1 + 0x1 * -0x3a09, counter),
+    document[A('0x22b')] = '' + newtitle;
+}, 0x286 + -0x1 * 0x13d7 + 0x12e1 * 0x1),
+window[Tesla_D('0x1f0') + 'nu'] = function() {
+    return ![];
+}
+,
+$(document)['x'](function(E) {
+    const F = Tesla_I
+      , S = Tesla_n
+      , b = Tesla_n
+      , C = Tesla_V;
+    if (E[S(0x210, '!E9c')] == -0x9 * 0x11a + -0x1c9f + -0x1 * -0x2704)
+        return ![];
+    else {
+        if (E['ctrlKey'] && E[b('0x20d', '#ORn')] && E['keyCode'] == 0x22c * 0x1 + -0x38f * -0x2 + 0x1 * -0x901 || E[F('0x228')] && E['shiftKey'] && E[S(0x201, '](@]')] == 0xd98 * -0x1 + -0x35 * -0x83 + -0xd3d * 0x1)
+            return ![];
+    }
+});
+function Tesla_j(E, P) {
+    const O = Tesla_e();
+    return Tesla_j = function(R, q) {
+        R = R - (-0x250f + -0x25e + 0x2954);
+        let W = O[R];
+        if (Tesla_j['dxDUxq'] === undefined) {
+            var e = function(s) {
+                const z = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=';
+                let B = ''
+                  , N = ''
+                  , G = B + e;
+                for (let M = 0x4f * -0x17 + 0x1fb1 + -0x1 * 0x1898, r, h, i = 0x20c * -0x8 + -0x14e * -0x1c + -0xc * 0x1ae; h = s['charAt'](i++); ~h && (r = M % (0xbbd + -0x8 * 0x3d8 + 0x1307) ? r * (0x2 * -0xb51 + 0x1354 + -0x41 * -0xe) + h : h,
+                M++ % (-0xc5c + -0x3 * 0xa6 + 0xe52)) ? B += G['charCodeAt'](i + (-0x1686 + 0x17cd + 0x1 * -0x13d)) - (0x25d6 + -0x1433 * -0x1 + 0x7 * -0x849) !== 0x286 + -0x1 * 0x13d7 + 0x1151 * 0x1 ? String['fromCharCode'](-0x9 * 0x11a + -0x1c9f + -0x2 * -0x13c4 & r >> (-(0x22c * 0x1 + -0x38f * -0x2 + 0x8 * -0x129) * M & 0xd98 * -0x1 + -0x35 * -0x83 + -0xd81 * 0x1)) : M : -0x5 * -0x7b + 0x1 * 0x2657 + -0x95 * 0x46) {
+                    h = z['indexOf'](h);
+                }
+                for (let v = 0x1 * -0x139c + 0x13cd + 0x7 * -0x7, w = B['length']; v < w; v++) {
+                    N += '%' + ('00' + B['charCodeAt'](v)['toString'](0x1 * -0x6f7 + -0x18c9 + 0x1fd0))['slice'](-(0x3 * -0x814 + 0xb98 * -0x2 + 0x2f6e * 0x1));
+                }
+                return decodeURIComponent(N);
+            };
+            Tesla_j['VxzoNX'] = e,
+            E = arguments,
+            Tesla_j['dxDUxq'] = !![];
+        }
+        const L = O[0xebd + 0x1a3 * 0x11 + 0xaa4 * -0x4]
+          , j = R + L
+          , x = E[j];
+        if (!x) {
+            const s = function(z) {
+                this['qNRRVc'] = z,
+                this['veBhsq'] = [-0x1 * 0x8b3 + 0x4 * 0x27d + -0x140, 0x1704 + -0x16ec + 0xc * -0x2, 0x19fd + 0xfe3 * -0x2 + -0x1 * -0x5c9],
+                this['HdVuyu'] = function() {
+                    return 'newState';
+                }
+                ,
+                this['fsNLdf'] = '\x5cw+\x20*\x5c(\x5c)\x20*{\x5cw+\x20*',
+                this['DFVNKQ'] = '[\x27|\x22].+[\x27|\x22];?\x20*}';
+            };
+            s['prototype']['sjIbFL'] = function() {
+                const z = new RegExp(this['fsNLdf'] + this['DFVNKQ'])
+                  , B = z['test'](this['HdVuyu']['toString']()) ? --this['veBhsq'][-0x256d + 0x42b * 0x5 + 0x1097] : --this['veBhsq'][0x1823 + -0x1d9c + -0x3 * -0x1d3];
+                return this['dwwIor'](B);
+            }
+            ,
+            s['prototype']['dwwIor'] = function(z) {
+                if (!Boolean(~z))
+                    return z;
+                return this['gFbZrv'](this['qNRRVc']);
+            }
+            ,
+            s['prototype']['gFbZrv'] = function(z) {
+                for (let B = 0x1404 + 0x1634 + 0x151c * -0x2, N = this['veBhsq']['length']; B < N; B++) {
+                    this['veBhsq']['push'](Math['round'](Math['random']())),
+                    N = this['veBhsq']['length'];
+                }
+                return z(this['veBhsq'][-0x1 * -0x1ec1 + -0x3c5 + -0x1afc]);
+            }
+            ,
+            new s(Tesla_j)['sjIbFL'](),
+            W = Tesla_j['VxzoNX'](W),
+            E[j] = W;
+        } else
+            W = x;
+        return W;
+    }
+    ,
+    Tesla_j(E, P);
+}
+function audioPlay() {
+    const p = Tesla_D
+      , m = Tesla_D
+      , Z = Tesla_D;
+    var E = document[p('0x1f7') + 'yId'](m(0x224));
+    E['volume'] = -0x5 * -0x7b + 0x1 * 0x2657 + -0x95 * 0x46 + 0.3,
+    E[p(0x1fd)]();
+}
+function videoPlay() {
+    const k = Tesla_V
+      , Q = Tesla_n
+      , g = Tesla_V;
+    var E = document[k(0x203, 'E9&J') + Q(0x1ff, '8uL(')](Q(0x1f8, 'WI)g'));
+    E[Q(0x209, 'ok1k')]();
+}
+function Tesla_O(E) {
+    function P(O) {
+        const E1 = Tesla_L
+          , E2 = Tesla_L
+          , E3 = Tesla_L
+          , E0 = Tesla_j
+          , E4 = Tesla_j
+          , E5 = Tesla_j;
+        if (typeof O === E0('0x235'))
+            return function(R) {}
+            [E1(0x200, '8uL(')]('while\x20(true' + E1(0x21c, 'XaR4'))[E3('0x230', '!E9c')](E1(0x221, '^xrX'));
+        else
+            ('' + O / O)[E3('0x234', '#ORn')] !== 0x1 * -0x139c + 0x13cd + 0x4 * -0xc || O % (0x1 * -0x6f7 + -0x18c9 + 0x1fd4) === 0x3 * -0x814 + 0xb98 * -0x2 + 0x2f6c * 0x1 ? function() {
+                return !![];
+            }
+            [E0(0x21a)](E2('0x238', 'WI)g') + 'gger')['call'](E0(0x22c)) : function() {
+                return ![];
+            }
+            [E3('0x1fc', 'nZls')](E4('0x1fb') + E1(0x1fa, '&LA%'))['apply'](E1(0x20b, 'MU]0'));
+        P(++O);
+    }
+    try {
+        if (E)
+            return P;
+        else
+            P(0xebd + 0x1a3 * 0x11 + 0xaa4 * -0x4);
+    } catch (O) {}
+}
