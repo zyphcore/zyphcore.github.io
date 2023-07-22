d3a9c8ab72f75f2870f22fd8eae5605ec194b87b
zyphcore <111462734+zyphcore@users.noreply.github.com>
2023-07-22 11:34:00 +0200

    Create typed.js

---
+++ z/typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+  strings : [,'private biolink ^_^','invite only lol :c','created by zyph :)','zypc *_^','so to myself c:','esex? :3','https://zyph.nl :D'],
+  typeSpeed : 35,
+  delaySpeed : 90,
+  showCursor: false,
+  loop : true
+})
