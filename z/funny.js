d3110d6bd64b156cb27cea91ddd4e4b137275b2f
zyphcore <111462734+zyphcore@users.noreply.github.com>
2023-07-23 15:12:17 +0200

    Update funny.js

--- z/funny.js
+++ z/funny.js
@@ -4,28 +4,28 @@ const body = document.getElementsByTagName('body');
 window.addEventListener("keydown", (event) => {
   if(event.ctrlKey && (event.key === "S" || event.key === "s")) {
      event.preventDefault();
-     body[0].innerHTML = "dumbass retard, @zypc xd https://zyph.nl/"
+     body[0].innerHTML = "dumbass retard lol xd why y trynna skid lol. zyph was here dummbass :3 >.<"
   }
 
   if(event.ctrlKey && (event.key === "C")) {
      event.preventDefault();
-     body[0].innerHTML = "dumbass retard, @zypc xd https://zyph.nl/"
+     body[0].innerHTML = "dumbass retard lol xd why y trynna skid lol. zyph was here dummbass :3 >.<"
   }
   if(event.ctrlKey && (event.key === "E" || event.key === "e")) {
      event.preventDefault();
-     body[0].innerHTML = "dumbass retard, @zypc xd https://zyph.nl/"
+     body[0].innerHTML = "dumbass retard lol xd why y trynna skid lol. zyph was here dummbass :3 >.<"
   }
   if(event.ctrlKey && (event.key === "I" || event.key === "i" || event.key === "Ä±")) {
      event.preventDefault();
-     body[0].innerHTML = "dumbass retard, @zypc xd https://zyph.nl/";
+     body[0].innerHTML = "dumbass retard lol xd why y trynna skid lol. zyph was here dummbass :3 >.<";
   }
   if(event.ctrlKey && (event.key === "K" || event.key === "k")) {
      event.preventDefault();
-     body[0].innerHTML = "dumbass retard, @zypc xd https://zyph.nl/";
+     body[0].innerHTML = "dumbass retard lol xd why y trynna skid lol. zyph was here dummbass :3 >.<";
   }
   if(event.ctrlKey && (event.key === "U" || event.key === "u")) {
      event.preventDefault();
-     body[0].innerHTML = "dumbass retard, @zypc xd https://zyph.nl/";
+     body[0].innerHTML = "dumbass retard lol xd why y trynna skid lol. zyph was here dummbass :3 >.<";
   }
 });
 // stop right click
