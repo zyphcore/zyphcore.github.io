780c39a6fd60a7bc7914b2934dc0b1192af3185d
zyphcore <111462734+zyphcore@users.noreply.github.com>
2023-08-14 14:26:12 +0200

    Create typed.js

---
+++ typed.js
@@ -0,0 +1,9 @@
+new Typed('#typed', {
+    strings: ['zyph is better >_<'],
+    typeSpeed: 120,
+    backSpeed: 120,
+    backDelay: 1000,
+    startDelay: 120,
+    loop: true,
+    showCursor: false
+});
