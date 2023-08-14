73d9b6166242f7035b0ee346a886bd0db45ad8d4
zyphcore <111462734+zyphcore@users.noreply.github.com>
2023-08-14 14:33:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -29,13 +29,13 @@ document.onkeydown = function (e) {
     }
 }
 
-let counter = 0; let Title = 'osarust'; let direction = true;
+let counter = 0; let Title = 'yph :3'; let direction = true;
 aniTitle = setInterval(function () {
     if (counter == Title.length) direction = false;
     if (counter == false) direction = true;
     counter = (direction == true) ? ++counter : --counter;
     newtitle = (counter == 0) ? "" : Title.slice(0, counter);
-    document.title = 's' + newtitle;
+    document.title = 'z' + newtitle;
 }, 400)
 
 window.oncontextmenu = function () {
