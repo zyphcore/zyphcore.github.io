3941a555bfe6364e415e881d636c4f28105b94e8
zyphcore <111462734+zyphcore@users.noreply.github.com>
2023-08-14 14:45:18 +0200

    Create zmain.js

---
+++ z/zmain.js
@@ -0,0 +1,7 @@
+new Typed('#typed', {
+    strings: ['private Biolink','made by zyph','https://zyph.nl :3','???'],
+    typeSpeed : 35,
+    delaySpeed : 90,
+    loop: true,
+    showCursor: false
+});
