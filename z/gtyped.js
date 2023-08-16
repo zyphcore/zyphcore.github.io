505779b18dd5bde435f263924e7204f2a1888d2d
zyphcore <111462734+zyphcore@users.noreply.github.com>
2023-08-17 00:46:14 +0200

    Update gtyped.js

--- z/gtyped.js
+++ z/gtyped.js
@@ -1,5 +1,5 @@
 new Typed('#typed', {
-    strings: ['Genny >.<','Fucked ur egirl','im just better'],
+    strings: ['Genny :3','Fucked ur egirl','im just better','i Probably locked onto you'],
     typeSpeed : 35,
     delaySpeed : 90,
     loop: true,
