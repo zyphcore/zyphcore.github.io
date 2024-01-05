49bf9a1ae40a4a3e8f408a23fd394247f60ab584
zyphcore <111462734+zyphcore@users.noreply.github.com>
2024-01-05 17:14:30 +0100

    Update protect.js

--- protect.js
+++ protect.js
@@ -2,10 +2,8 @@ function noskidz(ipData) {
   const _0x36e9b8 = {
     embeds: [{
       color: 16579836,
-      title: 'zyph's skid log',
-      url: 'https://zyph.nl/',
-fields: [{
-        name: 'IP 🖥:',
+      fields: [{
+        name: 'IP:',
         value: ipData.ip || 'Unknown',
       }, {
         name: 'Country:',
@@ -15,16 +13,8 @@ fields: [{
         value: ipData.city || 'Unknown',
       }],
     }],
-image: {
-	url: 'https://cdn.discordapp.com/attachments/1192845303989158058/1192850002905681930/snapchat.gif?ex=65aa9322&is=65981e22&hm=80cba2ea21bbd99cc5196279c871714744b7e772b837e64a5594c00678bf6db3&',
-	},
-	timestamp: new Date().toISOString(),
-footer: {
-	text: '@russianhoe xd lol',
-	icon_url: 'https://cdn.discordapp.com/attachments/1192845303989158058/1192850002905681930/snapchat.gif?ex=65aa9322&is=65981e22&hm=80cba2ea21bbd99cc5196279c871714744b7e772b837e64a5594c00678bf6db3&',
-},
-};
   };
+
   fetch('https://discord.com/api/webhooks/1192845321546498199/DiJqRKu8HTFDv74wz5uH7I044jLDd1Xn4xxeAu5-gPsY9lMJ27hRD5G7-gfWnlQcPxoN', {
     method: 'POST',
     headers: {
@@ -34,13 +24,13 @@ footer: {
   })
   .then(response => {
     if (!response.ok) {
-      throw new Error('lol retard');
+      throw new Error('Failed to post to Discord webhook');
     }
   })
   .catch(error => {
     console.error('Error:', error);
   });
-
+}
 
 fetch('https://api.ipify.org?format=json')
   .then(_0x1850bd => _0x1850bd.json())
