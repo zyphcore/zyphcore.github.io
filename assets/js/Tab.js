354d89baaa9f5a5f5247e19acb5742f2ca1990f0
zyph <111462734+zyphcore@users.noreply.github.com>
2025-07-15 15:11:34 +0200

    Delete assets directory

--- assets/js/Tab.js
+++
@@ -1,9 +0,0 @@
-const tabTitles = ['zyph', 'russianhoe', 'pupgirls'];
-let index = 0x0;
-function updateTabTitle() {
-  document.title = tabTitles[index];
-  index = (index + 0x1) % tabTitles.length;
-  console.clear()
-}
-updateTabTitle();
-setInterval(updateTabTitle, 0x1f4);
